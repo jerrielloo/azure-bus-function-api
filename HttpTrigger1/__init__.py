57edaea9633228b22a3f8e4129630e29ba1de7af
jerrielloo <jerrielloo@hotmail.com>
2022-11-11 18:14:16 +0100

    test

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -6,19 +6,29 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    busId = req.params.get('busId')
+    busStopId = req.params.get('busStopId')
+    timeStamps = req.params.get('timeStamps')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if busId and busStopId and timeStamps:
+        return "BusId: " + busId + "\nBusStopId: " + busStopId + "\nTimeStamps: " + timeStamps
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return "Some variables of these variables are missing: 'busId' or 'busStopId' or 'timeStamps' "
+
+
+
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
+
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
