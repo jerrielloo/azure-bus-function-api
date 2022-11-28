62739a7ace749560f89dbfa15d9ab40070e71a15
jerrielloo <jerrielloo@hotmail.com>
2022-11-28 13:12:29 +0100

    attempt to connect to database

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 import azure.functions as func
+import mysql.connector
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -10,10 +11,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     busStopId = req.params.get('busStopId')
     timeStamps = req.params.get('timeStamps')
 
-    if busId and busStopId and timeStamps:
-        return "BusId: " + busId + "\nBusStopId: " + busStopId + "\nTimeStamps: " + timeStamps
-    else:
-        return "Some variables of these variables are missing: 'busId' or 'busStopId' or 'timeStamps' "
+    host='dockerlab.westeurope.cloudapp.azure.com'
+    port=3306
+    username='CC_4'
+    password='1KwKNLcjP_132ngp_7kj4P5v775v8t5vQc-MQXXQjsQ'
+    database='CC_4'
+
+    cnx = mysql.connector.connect(user=username, password=password, host=host, database=database)
+
+    cursor = cnx.cursor()
+
+    print("------TESING CONNECTION TO DATABASE-----")
+    sql_str = "SELECT now();"
+    rs=cursor.executive(sql_str)
+    rs=cursor.fetchall()
+
+    return rs
+
+
+    # if busId and busStopId and timeStamps:
+    #     return "BusId: " + busId + "\nBusStopId: " + busStopId + "\nTimeStamps: " + timeStamps
+    # else:
+    #     return "Some variables of these variables are missing: 'busId' or 'busStopId' or 'timeStamps' "
 
 
 
