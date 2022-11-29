01d1e9f1c38e3650c11482ac622034c0c14a8970
jerrielloo <jerrielloo@hotmail.com>
2022-11-29 23:23:25 +0100

    changed to env variable

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -15,10 +15,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # host = 'dockerlab.westeurope.cloudapp.azure.com'
     host = os.environ["MYSQLCONNSTR_host"]
-    port=3306
-    username='CC_4'
-    password='1KwKNLcjP_132ngp_7kj4P5v775v8t5vQc-MQXXQjsQ'
-    database='CC_4'
+    username=os.environ["MYSQLCONNSTR_username"]
+    password=os.environ["MYSQLCONNSTR_password"]
+    database=os.environ["MYSQLCONNSTR_database"]
 
     try:
         cnx = mysql.connector.connect(user=username, password=password, host=host, database=database)
