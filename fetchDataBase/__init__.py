fd3ebcb32c0f48cb865c152d794677f074ce9783
jerrielloo <jerrielloo@hotmail.com>
2022-11-29 19:16:55 +0100

    test yet again, please please work

--- fetchDataBase/__init__.py
+++ fetchDataBase/__init__.py
@@ -8,7 +8,6 @@ import os
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-
     host='dockerlab.westeurope.cloudapp.azure.com'
     port=3306
     username='CC_4'
@@ -16,22 +15,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     database='CC_4'
 
     try:
-        # cnx = mysql.connector.connect(user=username, password=password, host=host, database=database)
+        cnx = mysql.connector.connect(user=username, password=password, host=host, database=database)
 
-        # cursor = cnx.cursor()
+        cursor = cnx.cursor()
 
-        # sql_str_station="SELECT * FROM Station"
-        # rs_station=cursor.execute(sql_str_station)
-        # rs_station =cursor.fetchall()
+        sql_str_station="SELECT * FROM Station"
+        rs_station=cursor.execute(sql_str_station)
+        rs_station =cursor.fetchall()
 
-        # sql_str_route="SELECT * FROM Route"
-        # rs_route =cursor.execute(sql_str_route)
-        # rs_route =cursor.fetchall()
+        sql_str_route="SELECT * FROM Route"
+        rs_route =cursor.execute(sql_str_route)
+        rs_route =cursor.fetchall()
 
-        return "hello"
+        return {"station": rs_station, "route": rs_route}
 
     except Exception as e:
-        return "error occurred"
+        return str(e)
 
 
     
